92b47686b1133fbef253eb85ff41e4110f4e6596
shoushuai <158483278@qq.com>
2017-02-21 08:35:40 +0800

    测试

---
+++ commonutils_lib/src/main/java/com/gss/commonutils_lib/Test.java
@@ -0,0 +1,11 @@
+package com.gss.commonutils_lib;
+
+/**
+ * Created by GSS on 2017/2/21.
+ */
+public class Test {
+
+    public double add1234(double a, double b) {
+        return a + b;
+    }
+}
