5a324253584ba2012ac308503d55e5032dc128f5
shoushuai <158483278@qq.com>
2017-03-23 23:23:48 +0800

    add statuslayout

---
+++ commonutils_lib/src/main/java/com/gss/commonutils_lib/statuslayout/OnRetryListener.java
@@ -0,0 +1,9 @@
+package com.gss.commonutils_lib.statuslayout;
+
+/**
+ * Created by GSS on 2017/3/23.
+ */
+public interface OnRetryListener {
+
+    void onRetry();
+}
---
+++ commonutils_lib/src/main/java/com/gss/commonutils_lib/statuslayout/OnShowHideViewListener.java
@@ -0,0 +1,12 @@
+package com.gss.commonutils_lib.statuslayout;
+
+import android.view.View;
+
+/**
+ * Created by GSS on 2017/3/23.
+ */
+public interface OnShowHideViewListener {
+
+    void onShowView(View view, int id);
+    void onHideView(View view, int id);
+}
